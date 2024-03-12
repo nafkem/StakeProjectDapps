3da13d7638ca5af3621b5c048d9fbaad53615e4d
AjayiMike <ajayimikeofficial@gmail.com>
2024-03-12 18:16:42 +0100

    completed

--- src/hooks/useNumberOfVoters.js
+++ src/hooks/useNumberOfVoters.js
@@ -1,29 +1,34 @@
-import { ethers } from "ethers";
 import { useCallback, useEffect, useState } from "react";
 import { wssProvider } from "../constants/providers";
+import { ethers } from "ethers";
 
 const useNumberOfVoters = () => {
     const [value, setValue] = useState(0);
 
-    const eventListerner = useCallback((log) => {
-        console.log("testing event: ", log);
+    const eventListerner = useCallback(() => {
+        setValue((prev) => prev + 1);
     }, []);
 
     useEffect(() => {
         const filter = {
             address: import.meta.env.VITE_ballot_contract_address,
-            topics: [ethers.id("GiveRightToVote(address,uint256)")],
+            topics: [
+                "0x02e8e9dbca99990e7fa2bd1a2cda8f76312b92ef3580255233e221484585545b",
+            ],
         };
-        wssProvider
-            .getLogs({ ...filter, fromBlock: 5465128 })
-            .then((events) => {
-                setValue(events.length + 1);
-            });
 
-        wssProvider.on(filter, eventListerner);
+        wssProvider.getLogs({ ...filter, fromBlock: 5465128 }).then((logs) => {
+            setValue(logs.length + 1);
+        });
 
-        return () => wssProvider.off(filter, eventListerner);
-    }, []);
+        const wssProvider2 = new ethers.WebSocketProvider(
+            import.meta.env.VITE_wss_rpc_url
+        );
+
+        wssProvider2.on(filter, eventListerner);
+
+        return () => wssProvider2.off(filter, eventListerner);
+    }, [eventListerner]);
 
     return value;
 };
